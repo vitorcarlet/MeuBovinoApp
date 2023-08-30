81efe37a51e49a4af9cb8ff05d37f342500aed98
vitorcarlet <vitor.carlet27@gmail.com>
2023-08-30 15:50:23 -0300

    criacao do projeto

---
+++ src/main/java/com/meubovinoapp/MeuBovinoAppApplication.java
@@ -0,0 +1,13 @@
+package com.meubovinoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuBovinoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuBovinoAppApplication.class, args);
+	}
+
+}
