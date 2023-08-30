4b262a86143891e9b1cafe2705993fe7b18da45e
vitorcarlet <vitor.carlet27@gmail.com>
2023-08-30 15:50:38 -0300

    user creation

---
+++ src/main/java/com/meubovinoapp/POJO/User.java
@@ -0,0 +1,30 @@
+package com.meubovinoapp.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column
+    private String name
+
+
+}
